969719d38c4306b852279e8e5119a1525e09269e
Dylan Whichard <dylan@whichard.com>
2016-03-27 18:29:37 -0400

    Read LICENSE from setup.py. Fixes #41

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ libfiles = []
 for x in os.walk('lib'):
     libfiles.append((os.path.join('/usr/lib/idiotic', *(x[0].split(os.path.sep)[1:])), [os.path.join(x[0], y) for y in x[2]]))
 
+def read_license():
+    with open("LICENSE") as f:
+        return f.read()
+
 setup(
     name='idiotic',
     packages=find_packages(exclude=['etc', 'contrib']),
@@ -16,28 +20,7 @@ setup(
     importantly developer-and-user-friendly solution for mashing together a wide
     assortment of existing home automation technologies into something which is
     useful as a whole.""",    
-    license="""The MIT License (MIT)
-
-    Copyright (c) 2015-2016 Hackafe and contributors
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-""",
+    license=read_license(),
     author='Dylan Whichard',
     author_email='dylan@whichard.com',
     url='https://github.com/umbc-hackafe/idiotic',
